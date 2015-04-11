41ed1513e25c929aa45a4f06a8ed639ebf2ee76c
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-04-11 19:24:49 +0200

    Facilities for loading properties files

---
+++ src/clj/config/parse.clj
@@ -0,0 +1,10 @@
+(ns config.parse
+  (:require [clojure.java.io :as io]))
+
+
+(defn load-props
+  [file-name]
+  (with-open [^java.io.Reader reader (clojure.java.io/reader file-name)] 
+    (let [props (java.util.Properties.)]
+      (.load props reader)
+      (into {} (for [[k v] props] [(keyword k) (read-string v)])))))
