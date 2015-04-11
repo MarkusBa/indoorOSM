871ecfbaec945e03ecebab1d50e088a132466e31
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-04-11 20:05:58 +0200

    Added core.typed, still does not check correctly

--- project.clj
+++ project.clj
@@ -4,11 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.typed "0.2.84"]
                  [com.theoryinpractise.frege/frege "3.21.586-g026e8d7"]
                  [compojure "1.1.8"]
                  [http-kit "2.1.16"]
                  [ring/ring-defaults "0.1.1"]]
-  :plugins [[lein-fregec "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-fregec "0.1.0-SNAPSHOT"]
+            [lein-typed "0.3.5"]]
   :frege-source-paths ["src/frege"]
+  :core.typed {:check [config.parse]}
   :source-paths ["src/clj"]
   )
--- src/clj/config/parse.clj
+++ src/clj/config/parse.clj
@@ -1,10 +1,18 @@
 (ns config.parse
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.core.typed :refer [ann Map] :as t]))
 
+;; use "lein typed check" to check this among other ns
 
+;;(ann load-props [String -> (Map clojure.lang.Keyword String)])
+(ann ^:no-check load-props [String -> (Map clojure.lang.Keyword String)])
 (defn load-props
   [file-name]
   (with-open [^java.io.Reader reader (clojure.java.io/reader file-name)] 
     (let [props (java.util.Properties.)]
       (.load props reader)
-      (into {} (for [[k v] props] [(keyword k) (read-string v)])))))
+      (let [keys (.keySet props)
+          values (.values props)]
+        (into {} (t/for [k :- String keys, v :- String values ] [(keyword k) (read-string v)]))))))
+
+
