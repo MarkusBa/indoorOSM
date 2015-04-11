7e3708f3a533022a58190b24e736471b800726f3
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-04-11 20:27:25 +0200

    Correction

--- src/clj/config/parse.clj
+++ src/clj/config/parse.clj
@@ -4,15 +4,12 @@
 
 ;; use "lein typed check" to check this among other ns
 
-;;(ann load-props [String -> (Map clojure.lang.Keyword String)])
 (ann ^:no-check load-props [String -> (Map clojure.lang.Keyword String)])
 (defn load-props
   [file-name]
   (with-open [^java.io.Reader reader (clojure.java.io/reader file-name)] 
     (let [props (java.util.Properties.)]
       (.load props reader)
-      (let [keys (.keySet props)
-          values (.values props)]
-        (into {} (t/for [k :- String keys, v :- String values ] [(keyword k) (read-string v)]))))))
+      (into {} (for [[k v] props] [(keyword k) (read-string v)])))))
 
 
