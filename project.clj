630dcbd43c414adf2df428f24149109e901799b7
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-04-12 05:36:52 +0200

    return json

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [;;[org.clojure/clojure "1.7.0-alpha6"]
-                 [org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.typed "0.2.84"]
                  [korma "0.4.0"]
                  [org.clojure/java.jdbc "0.3.5"] ;; workaround for korma
@@ -13,6 +12,7 @@
                  [ch.qos.logback/logback-classic "1.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [compojure "1.3.3"]
+                 [org.clojure/data.json "0.2.6"]
                  [ring "1.3.2"]]
   :plugins [[lein-fregec "0.1.0-SNAPSHOT"]
             [lein-typed "0.3.5"]
--- src/clj/routing/routes.clj
+++ src/clj/routing/routes.clj
@@ -4,19 +4,30 @@
             [compojure.core :refer [defroutes GET PUT DELETE POST]]
             [compojure.route :as route]
             [compojure.handler :as handler]
+            [clojure.data.json :as json]
             [clojure.tools.logging :as log]
-            [database.connect :as db]))
+            [database.connect :as db])
+  (:import (java.sql Date)))
 
 (defn index []
   (file-response "public/html/index.html" {:root "resources"}))
 
+(defn date-writer [key value]
+  (if (= key :ts)
+    (str (java.sql.Date. (.getTime value)))
+    value))
+
 (defn generate-response [data & [status]]
   {:status (or status 200)
    :headers {"Content-Type" "application/json"}
-   :body (pr-str data)})
+   :body (json/write-str data :value-fn date-writer )})
+
+(defn items []
+  (generate-response (db/get-items )))
 
 (defroutes routes
   (GET "/" [] (index))
+  (GET "/items" [] (items))
   (route/files "/" {:root "resources/public"}))
 
 (def app
